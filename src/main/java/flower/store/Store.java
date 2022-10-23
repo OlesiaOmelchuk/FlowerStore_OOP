d4fd87fdc171c5b56645a4b3e595637f63301d22
Olesia <olesyao2003@gmail.com>
2022-10-23 18:45:42 +0300

    added class store and tests

---
+++ src/main/java/flower/store/Store.java
@@ -0,0 +1,26 @@
+package flower.store;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+public class Store {
+    @Getter
+    private ArrayList<FlowerBucket> bucketsList = new ArrayList<FlowerBucket>();
+    public void addBucket(FlowerBucket bucket) {
+        bucketsList.add(bucket);
+    }
+    public ArrayList search(Flower targetFlower) {
+//        Returns list of buckets that contain given flower
+        ArrayList<FlowerBucket> desirableBuckets = new ArrayList<>();
+        for(FlowerBucket bucket: bucketsList){
+            for(FlowerPack pack: bucket.getPackList()){
+                if(pack.getFlower().getFlowerType() == targetFlower.getFlowerType()){
+                    desirableBuckets.add(bucket);
+                    break;
+                }
+            }
+        }
+        return desirableBuckets;
+    }
+}
---
+++ src/test/java/flower/store/StoreTest.java
@@ -0,0 +1,44 @@
+package flower.store;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+
+public class StoreTest {
+    private Store store;
+
+    @BeforeEach
+    public void init(){
+        store = new Store();
+    }
+
+    @Test
+    public void testSearch() {
+        Rose rose = new Rose();
+        Tulip tulip = new Tulip();
+        FlowerPack rosePack = new FlowerPack(rose, 5);
+        FlowerPack tulipPack = new FlowerPack(tulip, 7);
+//        bucket of roses
+        FlowerBucket bucketRose = new FlowerBucket();
+        bucketRose.add(rosePack);
+//        bucket of tulips
+        FlowerBucket bucketTulip = new FlowerBucket();
+        bucketTulip.add(tulipPack);
+//        bucket of roses and tulips
+        FlowerBucket bucketRoseTulip = new FlowerBucket();
+        bucketRoseTulip.add(rosePack);
+        bucketRoseTulip.add(tulipPack);
+//        add buckets to store
+        store.addBucket(bucketRose);
+        store.addBucket(bucketTulip);
+        store.addBucket(bucketRoseTulip);
+//        find all buckets with roses
+        ArrayList<FlowerBucket> expectedBuckets = new ArrayList<>();
+        expectedBuckets.add(bucketRose);
+        expectedBuckets.add(bucketRoseTulip);
+        Assertions.assertEquals(store.search(rose), expectedBuckets);
+    }
+
+}

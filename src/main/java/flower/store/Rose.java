75f84ff6cb67f54ec146bacf4e6e00274ca1bde1
Olesia <olesyao2003@gmail.com>
2022-10-23 15:21:30 +0300

    added classes for tulip and chamomile, fixed rose

--- src/main/java/flower/store/Rose.java
+++ src/main/java/flower/store/Rose.java
@@ -2,6 +2,6 @@ package flower.store;
 
 public class Rose extends Flower{
     public Rose(){
-        super(10, FlowerColor.RED, 20, FlowerType.ROSE);
+        setFlowerType(FlowerType.ROSE);
     }
 }
